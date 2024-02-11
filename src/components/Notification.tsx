e39e51199eb545ae01afafcaf6545bcd611c3f5c
MD.Rayhan Alam <140850073+RayhanAlamRangan@users.noreply.github.com>
2024-02-11 15:36:01 +0600

    forn-end-upload

---
+++ src/components/Notification.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div   className="h-12 bg-yellow-900 text-white px-4 md:text-baseline cursor-pointer items-center flex text-center justify-center text-sm">
+      Freedelivery for all orders over $50.Order your food now </div>
+  )
+}
+
+export default Notification
